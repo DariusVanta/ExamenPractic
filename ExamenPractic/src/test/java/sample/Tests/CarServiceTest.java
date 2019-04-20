8bc3e6c72955e91c94aaee33f89f44cabb7828dc
DariusVanta <vdci0169@scs.ubbcluj.ro>
2019-04-20 13:47:14 +0300

    Examen Practic

--- ExamenPractic/src/test/java/sample/Tests/CarServiceTest.java
+++ ExamenPractic/src/test/java/sample/Tests/CarServiceTest.java
@@ -13,9 +13,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CarServiceTest {
     @Test
-    void addShouldAddTheGivenInvoice() {
+    void addShouldAddTheGivenCar() {
 
-        InvoiceValidator validator = new InvoiceValidator();
+        CarValidator validator = new CarValidator();
         FileRepository<Car> repository = new FileRepository<>(validator, "test1.json", Car[].class);
         CarService service = new CarService(repository);
 
@@ -24,7 +24,6 @@ class CarServiceTest {
         assertEquals(1, all.size());
         assertEquals("1", all.get(0).getId());
         assertEquals("Fiat", all.get(0).getModel());
-
         assertEquals(123, all.get(0).getMileageAcquisition());
         assertEquals(5, all.get(0).getDayRentPrice());
 
@@ -32,26 +31,26 @@ class CarServiceTest {
 
     @Test
     void getDaySumShouldComputeCorrectDailySums() {
-        InvoiceValidator validator = new InvoiceValidator();
-        FileRepository<Invoice> repository = new FileRepository<>(validator, "test2.json", Invoice[].class);
-        InvoiceService service = new InvoiceService(repository);
+        CarValidator validator = new CarValidator();
+        FileRepository<Car> repository = new FileRepository<>(validator, "test2.json", Car[].class);
+        CarService service = new CarService(repository);
 
-        service.add("1", 261, "test", "20.04.2019");
-        service.add("2", 252, "test1", "21.04.2019");
-        service.add("3", 343, "test2", "20.04.2019");
-        service.add("4", 434, "test3", "20.04.2019");
-        service.add("5", 525, "test4", "21.04.2019");
-        service.add("6", 616, "test5", "22.04.2019");
+        service.add("1", "Dacia", 1224, 4 );
+        service.add("2", "Aro", 122333, 10);
+        service.add("3", "Lastun", 12121, 2);
+        service.add("4", "Dacia", 11111, 3);
+        service.add("5", "Dacia", 100000, 11);
+        service.add("6", "Aro", 20000, 200);
 
-        double sum20 = service.getDaySum("20.04.2019");
-        double sum21 = service.getDaySum("21.04.2019");
-        double sum22 = service.getDaySum("22.04.2019");
+        double milaq20 = service.getMileageAcquisition("Dacia");
+        double milaq21 = service.getMileageAcquisition("Aro");
+        double milaq22 = service.getMileageAcquisition("Lastun");
 
-        assertEquals(261+343+434, sum20);
-        assertEquals(252+525, sum21);
-        assertEquals(616, sum22);
+        assertEquals(1224+11111+20000, milaq20);
+        assertEquals(122333+100000, milaq21);
+        assertEquals(12121, milaq22);
 
-        assertThrows(InvoiceDateFormatException.class, () -> service.getDaySum("rewrwfsd"));
-        assertThrows(InvoiceDateFormatException.class, () -> service.getDaySum("63.13.2014"));
+        assertThrows(InvoiceDateFormatException.class, () -> service.getMileageAcquisition(0));
+        assertThrows(InvoiceDateFormatException.class, () -> service.getMileageAcquisition(-4445));
     }
 }
